367c7bff77998ee4f1726a1ed836fa91390f23ee
jay <joohong.kim@gmail.com>
2023-08-02 19:02:12 +0900

    chapt7-3

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    
   }
 
 function paintToDo(newTodo) {
@@ -18,7 +25,7 @@ function paintToDo(newTodo) {
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  
+
   toDoList.appendChild(li);
 }
 
@@ -26,7 +33,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
